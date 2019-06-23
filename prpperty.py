e0cf5a1e5907e49641bdcb346a09a66b84be7017
Leopold0801 <49242543+Leopold0801@users.noreply.github.com>
2019-06-23 14:18:42 +0800

    利用@property给一个Screen对象加上width和height属性，以及一个只读属性resolution

---
+++ prpperty.py
@@ -0,0 +1,26 @@
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+    @width.setter
+    def width(self, width_value):
+        self._width = width_value
+    @property
+    def height(self):
+        return self._height
+    @height.setter
+    def height(self, height_value):
+        self._height = height_value
+    @property
+    def resolution(self):
+        return self._width * self._height
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
