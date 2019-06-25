896ebaa9188a0fe58af535fcde83529136190679
Leopold0801 <49242543+Leopold0801@users.noreply.github.com>
2019-06-25 15:04:30 +0800

    多线程会改变线程间的变量，以及正确方法

---
+++ multithreading_can_change_value.py
@@ -0,0 +1,38 @@
+import time, threading
+
+# 假定这是你的银行存款:
+balance = 0
+lock = threading.Lock()
+
+def change_it(n):
+    # 先存后取，结果应该为0:
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+
+#此方法会因为线程的交替运行而导致值的改变，正确方法应该为正在运行的线程上锁，等待完成才进行下一个线程
+'''
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+'''
+#正确方法
+def run_thread(n):
+    for i in range(100000):
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            # 放心地改吧:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁:
+            lock.release()
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(balance)
